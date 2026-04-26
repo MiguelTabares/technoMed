c6adbbcee9af8e8af92934a14ca548ddb9de090f
MiguelTabares <silenceisbeautyofsoul@gmail.com>
2026-04-26 10:41:40 -0500

    @feature: Se implementa auth context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { authFirebase } from "../back/auth";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(authFirebase, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, loading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
